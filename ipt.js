dbf2c36a0aa70e04b2f1a8f9e08455505871aa3b
Jimsalm <116990067+Jimsalm@users.noreply.github.com>
2023-11-21 01:20:08 +0800

    Update ipt.js

--- ipt.js
+++ ipt.js
@@ -1,22 +1,45 @@
-
-    document.addEventListener("DOMContentLoaded", function() {
-    const darkModeToggle = document.getElementById("dark-mode-toggle");
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.getElementById("fs");
     const body = document.body;
     const header = document.querySelector("header");
     const footer2 = document.querySelector(".Footer2");
-    const darkModeIndicator = document.querySelector(".dark-mode-indicator");
+    const button = document.querySelector("button");
+    const wrapper = document.querySelector('.wrapper');
 
+    //show the loading screen
+    function showLoadingScreen() {
+        wrapper.style.display = 'block';
+    }
+
+    //hide the loading screen
+    function hideLoadingScreen() {
+        wrapper.style.display = 'none';
+    }
+
+    //dark mode toggle
     darkModeToggle.addEventListener("change", function () {
         if (darkModeToggle.checked) {
             body.classList.add("dark-mode");
             header.classList.add("dark-mode");
-            footer2.classList.add("dark-mode"); // Apply dark mode to Footer2
-            darkModeIndicator.textContent = "Dark";
+            footer2.classList.add("dark-mode");
         } else {
             body.classList.remove("dark-mode");
             header.classList.remove("dark-mode");
-            footer2.classList.remove("dark-mode"); // Remove dark mode from Footer2
-            darkModeIndicator.textContent = "Light";
+            footer2.classList.remove("dark-mode");
         }
+    });
+    
+    //"About Me" button
+    button.addEventListener('click', function() {
+        showLoadingScreen(); 
+        setTimeout(function() {
+            hideLoadingScreen(); 
+        }, 4000);
+    });
+
+    button.addEventListener('click', function() {
+        setTimeout(function(){
+            window.open('https://drive.google.com/uc?export=download&id=1KSUmlAqwm7jNjkyJfHPVrYi_sNSqPxwV','_blank');
+             },1800)
     });
 });
